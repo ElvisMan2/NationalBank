0fb36f2525f7186aecb2eda56eef14207302757a
ElvisMan2 <104602581+ElvisMan2@users.noreply.github.com>
2024-06-04 12:15:39 -0500

    suspender cuenta

--- js/gestionarCuenta.js
+++ js/gestionarCuenta.js
@@ -1,10 +1,41 @@
-document.getElementById('aperturarCuenta').addEventListener('click', async function() {
+document.getElementById('suspenderCuenta').addEventListener('click', ()=>{
     function getUserData() {
         const userData = localStorage.getItem('user');
         return userData ? JSON.parse(userData) : null;
     }
 
-    await htmlAjax('nueva-cuenta.html', 'info');
+    function populateAccountSelect() {
+        const user = getUserData();
+        
+        if (user && user.bankAccounts) {
+          const accountSelect = document.getElementById('accountSelect');
+          
+          user.bankAccounts.forEach(account => {
+            const option = document.createElement('option');
+            option.value = account.id;
+            option.textContent = `Cuenta: ${account.accountNumber} - Saldo: ${account.balance} - Estado: ${account.status}`;
+            accountSelect.appendChild(option);
+          });
+        } else {
+          console.error('No se encontraron cuentas bancarias para el usuario.');
+        }
+      }
+
+    htmlAjax('suspender-cuenta.html', 'info');
+    
+    setTimeout(() => {
+        populateAccountSelect();
+    }, 500); // Puedes ajustar este tiempo según sea necesario
+    
+});
+
+document.getElementById('aperturarCuenta').addEventListener('click',()=>{
+    function getUserData() {
+        const userData = localStorage.getItem('user');
+        return userData ? JSON.parse(userData) : null;
+    }
+
+    htmlAjax('nueva-cuenta.html', 'info');
     
     const user = getUserData();
     
@@ -14,11 +45,12 @@ document.getElementById('aperturarCuenta').addEventListener('click', async funct
         document.getElementById('nombres').value = user.firstName;
         document.getElementById('apellidos').value = user.lastName;
         document.getElementById('celular').value = user.phone;
-        document.getElementById('fecha-nacimiento').value = user.birthDate
-        ;
-        
-
+        document.getElementById('fecha-nacimiento').value = user.birthDate;
     }, 500); // Puedes ajustar este tiempo según sea necesario
 });
+
+
+
+
 
 
